96afb1d434bf7754c944607f4f9015aa77e32011
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-02-08 20:36:03 +0530

    Create pivot in rotated binary search.py

---
+++ pivot in rotated binary search.py
@@ -0,0 +1,25 @@
+def binarySearch(arr,start,end): 
+  
+  while start<=end:      
+    mid=int((start+end)/2) 
+    if(arr[mid+1]>arr[mid]): 
+      if (arr[start]<=arr[mid]): 
+        start = mid + 1
+      elif(arr[start]>=arr[mid]):
+        end = mid - 1
+    else:
+      print('pivot element is',mid+1)  
+    
+  
+  
+  
+s=int(input('enter size of array :'))
+
+print('enter elements of array in below:')
+arr = [int(x) for x in input().split()] 
+start=0
+end=s
+t=arr
+t.sort()
+if(s==len(arr) and t==arr):
+  binarySearch(arr,start,end) 
